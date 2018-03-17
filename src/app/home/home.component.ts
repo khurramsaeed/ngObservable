1ce78e02a60c9646de29c6057e4bc26dd5eb2537
khurramsaeedmalik <5n8BX6dp>
2018-03-17 20:39:07 +0100

    Unsubscribe

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,25 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/Rx';
 import { Observer } from 'rxjs/Observer';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+
+  ascObsSubscription: Subscription;
+  custObsSubscription: Subscription;
 
   constructor() { }
 
   ngOnInit() {
-    /* const ascNumbers = Observable.interval(1000);
+    const ascNumbers = Observable.interval(1000);
 
-    ascNumbers.subscribe(
+    this.ascObsSubscription = ascNumbers.subscribe(
       (number: number) => {
         console.log('Ascending number: ' + number);
       }
-    ); */
+    );
 
     const custObservable = Observable.create((observer: Observer<string>) => {
       setTimeout(() => {
@@ -29,14 +33,25 @@ export class HomeComponent implements OnInit {
         observer.next('second package');
       }, 4000);
       setTimeout(() => {
-        observer.error('Error occured');
+        // observer.error('Error occured');
+        observer.complete(); // packages sent after that won't arrive
       }, 5000);
+      setTimeout(() => {
+        observer.next('extra package');
+      }, 6000);
     });
-    custObservable.subscribe(
+    this.custObsSubscription = custObservable.subscribe(
       (data: string) => {console.log(data); },
       (error: string) => {console.log(error); },
       () => {console.log('Completed'); }
     );
+  }
+
+  // Unscribe on destruction of this component
+  // Example: When another component replaces this component
+  ngOnDestroy(): void {
+    this.ascObsSubscription.unsubscribe();
+    this.custObsSubscription.unsubscribe();
   }
 
 }
